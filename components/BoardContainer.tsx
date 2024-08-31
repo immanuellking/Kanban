d4d44893299bb0900e8abbf3384473d71b584561
immanuellking <emmaebubeegbosi@gmail.com>
2024-08-31 20:18:39 +0100

    added condition for displaying board or signin button if user is authenticated or not

---
+++ components/BoardContainer.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+
+export default function BoardContainer() {
+  const { isSignedIn } = useUser();
+  const { push } = useRouter();
+
+  return (
+    <section className="w-full h-full  bg-[#20212C]">
+      {!isSignedIn ? (
+        <div className="w-full h-full flex flex-col justify-center items-center gap-y-4">
+          <h4 className="text-xl font-semibold text-[#828fa3]">
+            Please sign in to enjoy the Kanban app
+          </h4>
+          <button
+            className="bg-[#635FC7] w-[20rem] py-3 text-white font-semibold rounded-full"
+            onClick={() => push("/login")}
+          >
+            Login Now
+          </button>
+        </div>
+      ) : (
+        <div>Board Container</div>
+      )}
+    </section>
+  );
+}
