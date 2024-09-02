6a314a21d6967e291fe38941fbc5dc856f006015
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-02 15:50:16 +0100

    passed isSigned to main board to display login component when user notb authenticated

--- components/MainBoard.tsx
+++ components/MainBoard.tsx
@@ -1,11 +1,11 @@
 import BoardContainer from "./BoardContainer";
 import BoardHeader from "./BoardHeader";
 
-export default function MainBoard() {
+export default function MainBoard({ isSignedIn }: { isSignedIn: boolean }) {
   return (
     <main className="w-full h-screen flex flex-col">
       <BoardHeader />
-      <BoardContainer />
+      <BoardContainer isSignedIn={isSignedIn} />
     </main>
   );
 }
