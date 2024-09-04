b9c46d39321fc8773151383c0184a878ac5563b1
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-04 02:31:48 +0100

    cahnged file name, removed trigger and used context to handke modal state

---
+++ components/CreateNewBoardModal.tsx
@@ -0,0 +1,34 @@
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { AddNewBoardForm } from "./AddNewBoardForm";
+import { useDialog } from "@/context/dialogContext";
+
+export default function CreateNewBoardModal() {
+  const { state, closeDialog } = useDialog();
+  return (
+    <Dialog
+      onOpenChange={closeDialog}
+      open={state.isOpen}
+      modal
+      defaultOpen={state.isOpen}
+    >
+      <DialogContent className="bg-[#2B2C37] border-none" aria-description="">
+        <DialogHeader>
+          <DialogTitle className="text-xl text-white">
+            Add New Board
+          </DialogTitle>
+          <DialogDescription />
+        </DialogHeader>
+        <div>
+          <AddNewBoardForm />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
