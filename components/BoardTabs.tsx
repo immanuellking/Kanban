355efa2f2cc10f2c3c9478e520cd649dd072fab3
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-04 02:39:13 +0100

    added button with opening modal functionality

--- components/BoardTabs.tsx
+++ components/BoardTabs.tsx
@@ -1,7 +1,9 @@
+"use client";
+import { useDialog } from "@/context/dialogContext";
 import React from "react";
-import CreateNewBoardButton from "./CreateNewBoardButton";
 
 export default function BoardTabs() {
+  const { openDialog } = useDialog();
   return (
     <div className="mt-[5rem] space-y-4 flex-1 h-full">
       <span className="pl-6 text-gray-400 text-sm">All Boards (2)</span>
@@ -30,7 +32,30 @@ export default function BoardTabs() {
             <p className="text-base font-medium">Testing 1</p>
           </li>
         </ul>
-        <CreateNewBoardButton />
+        <button
+          className="flex items-center gap-x-2 text-[#635FC7] py-4 px-6 mt-2 rounded-r-full cursor-pointer"
+          onClick={() => openDialog()}
+        >
+          <svg
+            stroke="currentColor"
+            fill="none"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="h-[1.5rem] w-[1.5rem]"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
+            <path d="M4 12h8"></path>
+            <path d="M12 15h8"></path>
+            <path d="M12 9h8"></path>
+            <path d="M12 4v16"></path>
+          </svg>
+          <p className="text-base font-medium">Create New Board</p>
+        </button>
       </div>
     </div>
   );
