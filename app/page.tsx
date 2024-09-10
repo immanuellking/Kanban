73d9a9ca6e2402c335f3dd51386871afafc9113b
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-10 19:23:46 +0100

    destructured search params from page and pass to the kanban as props

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Kanban from "@/components/Kanban";
 
-export default function Home() {
-  return <Kanban />;
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { board: string };
+}) {
+  const boardQuery = searchParams.board || "";
+
+  return <Kanban boardQuery={boardQuery} />;
 }
