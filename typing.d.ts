3a1aa1fae5c6ac6dd8415f769c87688f688726a9
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-10 02:14:23 +0100

    created types for boardData and column

--- typing.d.ts
+++ typing.d.ts
@@ -1,4 +1,16 @@
 type BoardTab = {
-    user_id: string,
-    board_name: string
-}
+  user_id: string;
+  board_name: string;
+};
+
+type Column = {
+  _id: string;
+  column_name: string;
+  user_id: string;
+  board_id: string;
+};
+
+type BoardData = {
+  _id: string;
+  columns: Column[];
+};
