52f19fd63bbe69f9b82848e354c864e71417a294
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-12 23:53:07 +0100

    implemented function for generating random colors for column title

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getRandomBrightColor(): string {
+  // Helper function to generate a random integer between min and max values
+  const randomInt = (min: number, max: number) =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+  // Ensure at least one component is high (near 255) to keep the color bright
+  const r = randomInt(30, 255); // Random value between 128 and 255
+  const g = randomInt(118, 255); // Random value between 128 and 255
+  const b = randomInt(150, 255); // Random value between 128 and 255
+
+  // Randomly shuffle RGB values to get diverse bright colors
+  const rgb = [r, g, b].sort(() => 0.5 - Math.random());
+
+  // Return the RGB color as a string
+  return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+}
