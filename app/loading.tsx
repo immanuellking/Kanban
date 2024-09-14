4be0a6db67dc00a478a61fc2ff65a12efcbf0e97
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-14 13:31:01 +0100

    laoding for page streaming when getting the board data

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className="fixed z-50 top-0 right-0 left-0 bottom-0 bg-[#20212C] w-full h-full flex items-center justify-center">
+      <div className="bars-6"></div>
+    </div>
+  );
+}
