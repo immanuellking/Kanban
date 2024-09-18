b89921f55cbbd0f060aa9320d2ba6be92eb3e00c
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-18 10:28:11 +0100

    creted component abstraction for section for adding new column

---
+++ components/AddNewColumnButton.tsx
@@ -0,0 +1,15 @@
+import { useDialog } from "@/context/dialogContext";
+
+export default function AddNewColumnButton() {
+  const { openNewColumnDialog } = useDialog();
+  return (
+    <div
+      className="group bg-[#22232E]  h-full w-[18rem] rounded-sm flex items-center justify-center cursor-pointer"
+      onClick={() => openNewColumnDialog()}
+    >
+      <button className="text-2xl font-semibold text-[rgb(130,143,163)] group-hover:text-[#635FC7] transition-all ease-in duration-150">
+        + New Column
+      </button>
+    </div>
+  );
+}
