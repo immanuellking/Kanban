fa3a574e8497437e3fb6639e2123de404b733f81
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-18 10:36:09 +0100

    task components ui

---
+++ components/ColumnTask.tsx
@@ -0,0 +1,17 @@
+export default function ColumnTask({ task }: { task: Task }) {
+  const done = task.subTasks.reduce(
+    (acc, curr) => (curr.is_complete ? acc + 1 : acc),
+    0
+  );
+
+  return (
+    <div className="group bg-[#2B2C37] h-24 flex flex-col justify-center px-4 rounded-sm space-y-2 cursor-pointer shadow-[0px_4px_6px_0px_rgba(54,78,126,0.10)]">
+      <h2 className="text-white capitalize text-lg font-semibold m-0 p-0 group-hover:text-[#635fc7] transition-all ease-linear duration-150">
+        {task.title}
+      </h2>
+      <p className="text-gray-400 text-[13px] m-0 p-0">
+        {done} of {task.subTasks.length} subtasks
+      </p>
+    </div>
+  );
+}
