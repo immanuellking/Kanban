52baac1f6b1aab5e008582996c14fcce62c67f4b
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-18 10:26:58 +0100

    moved section of code into abstractions and imported them

--- components/BoardContainer.tsx
+++ components/BoardContainer.tsx
@@ -4,6 +4,11 @@ import { getRandomBrightColor } from "@/lib/utils";
 import AddNewColumnModal from "./AddNewColumnModal";
 import { useDialog } from "@/context/dialogContext";
 import { useEffect, useState } from "react";
+import EmptyBoard from "./EmptyBoard";
+import LoginNow from "./LoginNow";
+import AddNewColumnButton from "./AddNewColumnButton";
+import ColumnTask from "./ColumnTask";
+import ColumnTasks from "./ColumnTasks";
 
 export default function BoardContainer({
   isSignedIn,
@@ -23,7 +28,7 @@ export default function BoardContainer({
 
   useEffect(() => {
     const params = new URLSearchParams(searchParams);
-    if (params.get("board") === boardData[0]?.board_name  ) {
+    if (params.get("board") === boardData[0]?.board_name) {
       setIsLoading(false);
     }
   }, [boardData, searchParams]);
@@ -32,24 +37,14 @@ export default function BoardContainer({
     <>
       <section className="w-full h-full bg-[#20212C]">
         {!isSignedIn ? (
-          <div className="w-full h-full flex flex-col justify-center items-center gap-y-8">
-            <h4 className="text-xl font-semibold text-[#828fa3]">
-              Please sign in to enjoy the Kanban app
-            </h4>
-            <button
-              className="bg-[#635FC7] w-[20rem] hover:bg-[#a8a4ff] transition-all ease-in-out duration-500 py-4 text-white font-semibold rounded-full"
-              onClick={() => push("/login")}
-            >
-              Login Now
-            </button>
-          </div>
+          <LoginNow />
         ) : (
           <div className="overflow-x-auto no-scrollbar flex h-full">
             {boardData.length > 0 ? (
               <div className="h-full pt-6 pb-4 px-6 flex gap-4">
                 {boardData[0]?.columns.length > 0
                   ? boardData[0]?.columns.map((column) => (
-                      <div key={column._id} className="h-full w-[18rem]">
+                      <div key={column._id} className="h-full w-[18rem] space-y-6">
                         <div className="flex items-center gap-x-2">
                           {isClient && (
                             <div
@@ -63,31 +58,14 @@ export default function BoardContainer({
                             {column.column_name}
                           </p>
                         </div>
+                        <ColumnTasks tasks={column.tasks} />
                       </div>
                     ))
                   : null}
-                <div
-                  className="group bg-[#22232E]  h-full w-[18rem] rounded-sm flex items-center justify-center cursor-pointer"
-                  onClick={() => openNewColumnDialog()}
-                >
-                  <button className="text-2xl font-semibold text-[rgb(130,143,163)] group-hover:text-[#635FC7] transition-all ease-in duration-150">
-                    + New Column
-                  </button>
-                </div>
+                <AddNewColumnButton />
               </div>
             ) : (
-              <div className="w-full h-full flex flex-col justify-center items-center gap-y-8">
-                <h4 className="text-xl font-semibold text-[#828fa3]">
-                  You currently don't have any boards. Create a new board to get
-                  started
-                </h4>
-                <button
-                  className="bg-[#635FC7] w-[35rem] hover:bg-[#a8a4ff] transition-all ease-in-out duration-500 py-4 text-white font-semibold rounded-full"
-                  onClick={() => openDialog()}
-                >
-                  + Create New Board
-                </button>
-              </div>
+              <EmptyBoard />
             )}
           </div>
         )}
