35a25ea77a67748a0cf7f6d1df4674faf63a87fd
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-26 01:08:57 +0100

    added property to resolve next js warning

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="pl-6">
-      <Image src="/logo-light.svg" alt="logo" width={155} height={26} />
+      <Image
+        src="/logo-light.svg"
+        alt="logo"
+        width={155}
+        priority
+        height={26}
+      />
     </div>
   );
 }
