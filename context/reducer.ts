e0cd6beb3de24e911df6d479e490db9786c1e3bf
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-26 01:02:42 +0100

    implemented reducer case for setting editing board mmode and data

--- context/reducer.ts
+++ context/reducer.ts
@@ -1,33 +1,53 @@
 export const reducer = (state: StateType, action: ActionType) => {
-    switch (action.type) {
-      case "OPEN_DIALOG":
-        return { ...state, isOpen: true };
-  
-      case "CLOSE_DIALOG":
-        return { ...state, isOpen: false };
-  
-      case "OPEN_NEW_COLUMN_DIALOG":
-        return { ...state, isAddNewColumnOpen: true };
-  
-      case "CLOSE_NEW_COLUMN_DIALOG":
-        return { ...state, isAddNewColumnOpen: false };
-  
-      case "OPEN_NEW_TASK_DIALOG":
-        return { ...state, isAddNewTaskOpen: true, isEditingTask: action.payload };
-  
-      case "CLOSE_NEW_TASK_DIALOG":
-        return { ...state, isAddNewTaskOpen: false, isEditingTask: false };
-  
-      case "OPEN_VIEW_TASK_DIALOG":
-        return { ...state, viewTaskOpen: true, task: action.payload };
-  
-      case "CLOSE_VIEW_TASK_DIALOG":
-        return { ...state, viewTaskOpen: false, };
-  
-      case "TOGGLE_LOADING_STATE":
-        return { ...state, isLoading: action.payload };
-  
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case "OPEN_DIALOG":
+      return { ...state, isOpen: true };
+
+    case "CLOSE_DIALOG":
+      return { ...state, isOpen: false };
+
+    case "OPEN_NEW_COLUMN_DIALOG":
+      return { ...state, isAddNewColumnOpen: true };
+
+    case "CLOSE_NEW_COLUMN_DIALOG":
+      return { ...state, isAddNewColumnOpen: false };
+
+    case "OPEN_NEW_TASK_DIALOG":
+      return {
+        ...state,
+        isAddNewTaskOpen: true,
+        isEditingTask: action.payload,
+      };
+
+    case "CLOSE_NEW_TASK_DIALOG":
+      return { ...state, isAddNewTaskOpen: false, isEditingTask: false };
+
+    case "OPEN_VIEW_TASK_DIALOG":
+      return { ...state, viewTaskOpen: true, task: action.payload };
+
+    case "CLOSE_VIEW_TASK_DIALOG":
+      return { ...state, viewTaskOpen: false };
+
+    case "TOGGLE_LOADING_STATE":
+      return { ...state, isLoading: action.payload };
+
+    case "OPEN_EDIT_BOARD":
+      return {
+        ...state,
+        isOpen: true,
+        isEditingBoard: true,
+        board: action.payload,
+      };
+
+    case "CLOSE_EDIT_BOARD":
+      return {
+        ...state,
+        isOpen: false,
+        isEditingBoard: false,
+        board: null,
+      };
+
+    default:
+      return state;
+  }
+};
