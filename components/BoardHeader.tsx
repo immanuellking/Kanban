85c9d97cb6325bc4b205d9267c94b498425b52cb
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-27 01:31:52 +0100

    moved signout button section to a new component use session Id to reneder sign out component

--- components/BoardHeader.tsx
+++ components/BoardHeader.tsx
@@ -5,11 +5,14 @@ import React from "react";
 import AddNewTaskModal from "./AddNewTaskModal";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import DeleteBoardModal from "./DeleteBoardModal";
+import { useAuth } from "@clerk/nextjs";
+import SignOut from "./SignOut";
 
 export default function BoardHeader({ boardData }: { boardData: BoardData[] }) {
   const searchParams = useSearchParams();
   const params = new URLSearchParams(searchParams);
   const currentTab = params.get("board");
+  const { sessionId } = useAuth();
 
   const { openNewTaskDialog, openEditBoard, openDeleteBoard } = useDialog();
 
@@ -31,7 +34,7 @@ export default function BoardHeader({ boardData }: { boardData: BoardData[] }) {
 
   return (
     <>
-      <div className="w-full flex items-center justify-between px-10 py-6 bg-[#2B2C37]">
+      <div className="w-full flex items-center justify-between px-10 h-[13vh] bg-[#2B2C37]">
         <div>
           <h1 className="text-2xl text-white font-semibold capitalize">
             {currentTab}
@@ -39,25 +42,9 @@ export default function BoardHeader({ boardData }: { boardData: BoardData[] }) {
         </div>
 
         <div className="flex gap-x-12">
-          <button>
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 24 24"
-              className="text-[2rem] text-[#828fa3]"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="Logout">
-                <g>
-                  <path d="M20.968,18.448a2.577,2.577,0,0,1-2.73,2.5c-2.153.012-4.306,0-6.459,0a.5.5,0,0,1,0-1c2.2,0,4.4.032,6.6,0,1.107-.016,1.589-.848,1.589-1.838V5.647A1.546,1.546,0,0,0,19,4.175a3.023,3.023,0,0,0-1.061-.095H11.779a.5.5,0,0,1,0-1c2.224,0,4.465-.085,6.687,0a2.567,2.567,0,0,1,2.5,2.67Z"></path>
-                  <path d="M3.176,11.663a.455.455,0,0,0-.138.311c0,.015,0,.028-.006.043s0,.027.006.041a.457.457,0,0,0,.138.312l3.669,3.669a.5.5,0,0,0,.707-.707L4.737,12.516H15.479a.5.5,0,0,0,0-1H4.737L7.552,8.7a.5.5,0,0,0-.707-.707Z"></path>
-                </g>
-              </g>
-            </svg>
-          </button>
+          {sessionId && (
+            <SignOut />
+          )}
 
           <div className="flex items-center gap-x-2">
             <button
