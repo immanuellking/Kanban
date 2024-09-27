9b4bfc092aa8c96badcd77e58579ca7a554e0bdc
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-27 01:35:39 +0100

    rafctored and moved conatiner code while also moving each column code into their component

---
+++ components/ColumnContainers.tsx
@@ -0,0 +1,25 @@
+import AddNewColumnButton from "./AddNewColumnButton";
+import Column from "./Column";
+import EmptyBoard from "./EmptyBoard";
+
+export default function ColumnContainers({
+  boardData,
+}: {
+  boardData: BoardData[];
+}) {
+  if (!boardData.length) {
+    return <EmptyBoard />;
+  }
+
+  if (boardData.length > 0) {
+    const columns = boardData[0].columns || [];
+    return (
+      <div className="h-full pt-6 pb-0 px-6 flex gap-4 overflow-hidden">
+        {columns.length > 0
+          ? columns.map((column) => <Column key={column._id} column={column} />)
+          : null}
+        <AddNewColumnButton />
+      </div>
+    );
+  }
+}
