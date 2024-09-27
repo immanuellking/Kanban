202e40237820d42b5ca5ffa6d868e16087369d72
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-28 00:07:53 +0100

    added logo and board title section in eader that toggles the board tabs mobile modal

--- components/BoardHeader.tsx
+++ components/BoardHeader.tsx
@@ -7,6 +7,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import DeleteBoardModal from "./DeleteBoardModal";
 import { useAuth } from "@clerk/nextjs";
 import SignOut from "./SignOut";
+import Image from "next/image";
 
 export default function BoardHeader({ boardData }: { boardData: BoardData[] }) {
   const searchParams = useSearchParams();
@@ -14,7 +15,12 @@ export default function BoardHeader({ boardData }: { boardData: BoardData[] }) {
   const currentTab = params.get("board");
   const { sessionId } = useAuth();
 
-  const { openNewTaskDialog, openEditBoard, openDeleteBoard } = useDialog();
+  const {
+    openNewTaskDialog,
+    openEditBoard,
+    openDeleteBoard,
+    openMobileBoardTabs,
+  } = useDialog();
 
   const columns = boardData[0]?.columns;
 
@@ -34,21 +40,47 @@ export default function BoardHeader({ boardData }: { boardData: BoardData[] }) {
 
   return (
     <>
-      <div className="w-full flex items-center justify-between px-10 h-[13vh] bg-[#2B2C37]">
-        <div>
-          <h1 className="text-2xl text-white font-semibold capitalize">
+      <div className="w-full flex items-center justify-between px-4 sm:px-6 lg:px-10 h-[10vh] lg:h-[13vh] bg-[#2B2C37]">
+        <>
+          <div
+            className="flex sm:hidden items-center"
+            onClick={() => openMobileBoardTabs()}
+          >
+            <div className="block sm:hidden relative w-[1.3rem] h-[1.3rem] mr-2">
+              <Image src="/logo-mobile.svg" alt="logo" fill />
+            </div>
+            <h1 className="text-xl sm:text-2xl text-white font-semibold capitalize">
+              {currentTab}
+            </h1>
+            <span className="block sm:hidden ml-2">
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 16 16"
+                className="text-[#635fc7]"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+                ></path>
+              </svg>
+            </span>
+          </div>
+          <h1 className="hidden sm:block text-xl sm:text-2xl text-white font-semibold capitalize">
             {currentTab}
           </h1>
-        </div>
+        </>
 
-        <div className="flex gap-x-12">
-          {sessionId && (
-            <SignOut />
-          )}
+        <div className="flex gap-x-6 sm:gap-x-12">
+          {sessionId && <SignOut />}
 
           <div className="flex items-center gap-x-2">
             <button
-              className="flex text-white font-semibold bg-[#635FC7] py-4 px-8 rounded-full disabled:cursor-not-allowed disabled:opacity-60"
+              className="flex text-white font-semibold bg-[#635FC7] py-2 px-3.5 sm:py-4 sm:px-8 rounded-full disabled:cursor-not-allowed disabled:opacity-60"
               disabled={!boardData.length}
               onClick={() => openNewTaskDialog(false)}
             >
@@ -66,7 +98,7 @@ export default function BoardHeader({ boardData }: { boardData: BoardData[] }) {
               >
                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
               </svg>
-              Add New Task
+              <span className="hidden sm:block">Add New Task</span>
             </button>
 
             <Popover>
