43bc47cc57385083454236ac101d51af372503c9
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-28 00:37:00 +0100

    added mobile board modal to kanbabn and passes in the boardTabs data

--- components/Kanban.tsx
+++ components/Kanban.tsx
@@ -1,6 +1,7 @@
 import SideBar from "./SideBar";
 import MainBoard from "./MainBoard";
 import { createNewUser, getBoardTabs, getBoardData } from "@/lib/data";
+import MobileBoardTabs from "./MobileBoardTabs";
 
 async function Kanban({ boardQuery }: { boardQuery: string }) {
   const isSignedIn = await createNewUser();
@@ -8,10 +9,13 @@ async function Kanban({ boardQuery }: { boardQuery: string }) {
   const boardData = await getBoardData(boardQuery);
 
   return (
-    <main className="flex h-screen overflow-hidden">
-      <SideBar boardTabs={boardTabs} />
-      <MainBoard isSignedIn={isSignedIn} boardData={boardData} />
-    </main>
+    <>
+      <main className="flex h-screen overflow-hidden">
+        <SideBar boardTabs={boardTabs} />
+        <MainBoard isSignedIn={isSignedIn} boardData={boardData} />
+      </main>
+      <MobileBoardTabs boardTabs={boardTabs} />
+    </>
   );
 }
 
