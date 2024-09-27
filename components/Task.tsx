0e2122d53c09b0f5738bf185725bb9fa1caca824
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-27 02:05:36 +0100

    remove react fragment cus its only one child element

--- components/Task.tsx
+++ components/Task.tsx
@@ -8,18 +8,16 @@ export default function Task({ task }: { task: Task }) {
   const done = subtaskTotal(task.subTasks);
 
   return (
-    <>
-      <div
-        onClick={() => openViewTaskDialog(task)}
-        className="group bg-[#2B2C37] h-24 flex flex-col justify-center px-4 rounded-sm space-y-2 cursor-pointer shadow-[0px_4px_6px_0px_rgba(54,78,126,0.10)]"
-      >
-        <h2 className="text-white capitalize text-lg font-semibold m-0 p-0 group-hover:text-[#635fc7] transition-all ease-linear duration-150 line-clamp-1">
-          {task.title}
-        </h2>
-        <p className="text-gray-400 text-[13px] m-0 p-0">
-          {done} of {task.subTasks.length} subtasks
-        </p>
-      </div>
-    </>
+    <div
+      onClick={() => openViewTaskDialog(task)}
+      className="group bg-[#2B2C37] h-24 flex flex-col justify-center px-4 rounded-sm space-y-2 cursor-pointer shadow-[0px_4px_6px_0px_rgba(54,78,126,0.10)]"
+    >
+      <h2 className="text-white capitalize text-lg font-semibold m-0 p-0 group-hover:text-[#635fc7] transition-all ease-linear duration-150 line-clamp-1">
+        {task.title}
+      </h2>
+      <p className="text-gray-400 text-[13px] m-0 p-0">
+        {done} of {task.subTasks.length} subtasks
+      </p>
+    </div>
   );
 }
