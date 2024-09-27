8af2d21d613476468c2f21afda6321f6885a068a
immanuellking <emmaebubeegbosi@gmail.com>
2024-09-28 00:29:04 +0100

    remove overflo hidden, not useful

--- components/ColumnContainers.tsx
+++ components/ColumnContainers.tsx
@@ -14,7 +14,7 @@ export default function ColumnContainers({
   if (boardData.length > 0) {
     const columns = boardData[0].columns || [];
     return (
-      <div className="h-full pt-6 pb-0 px-6 flex gap-4 overflow-hidden">
+      <div className="h-full pt-6 pb-0 px-6 flex gap-4">
         {columns.length > 0
           ? columns.map((column) => <Column key={column._id} column={column} />)
           : null}
