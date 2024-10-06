fd93c1f54b3b517ea212bd22a44c8ab0e397ad35
immanuellking <emmaebubeegbosi@gmail.com>
2024-10-06 22:51:54 +0100

    corrected file path and made style changes to the toast component

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border bg-[#20212C] text-green-400 border-none outline-0",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
