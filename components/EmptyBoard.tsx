7b925b1fd449ed0de1a7546fdbef7f01e5f11fde
immanuellking <emmaebubeegbosi@gmail.com>
2024-11-04 10:24:33 +0100

    resolved

--- components/EmptyBoard.tsx
+++ components/EmptyBoard.tsx
@@ -5,7 +5,7 @@ export default function EmptyBoard() {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-y-6">
       <h4 className="text-xl font-semibold text-[#828fa3]">
-        You currently don't have any boards. Create a new board to get started
+        You currently don&apos;t have any boards. Create a new board to get started
       </h4>
       <button
         className="bg-[#635FC7] w-[35rem] hover:bg-[#a8a4ff] transition-all ease-in-out duration-500 py-3.5 text-white font-semibold rounded-full"
