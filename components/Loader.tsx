d981da2e89839de25d04d735d1605f6a4aaf9fcf
immanuellking <emmaebubeegbosi@gmail.com>
2024-11-08 20:29:00 +0100

    reduce zindex on loading to show alert

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,6 +1,6 @@
 export const Loader = () => {
   return (
-    <div className="fixed z-[1000] top-0 right-0 left-0 bottom-0 bg-black/70 w-full h-full flex items-center justify-center">
+    <div className="fixed z-[100] top-0 right-0 left-0 bottom-0 bg-black/70 w-full h-full flex items-center justify-center">
       <div className="bars-6"></div>
     </div>
   );
