147f0689a6ce0cc8eb8fc45f8e0647e1961e9ae1
immanuellking <emmaebubeegbosi@gmail.com>
2024-12-13 11:49:08 +0100

    implemented drag leave, over and drop on the column for the drag element and also called the action to handle the update

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,9 +1,31 @@
+import { updateDropColumn } from "@/lib/action";
 import ColumnTasks from "./ColumnTasks";
 import { getRandomBrightColor } from "@/lib/utils";
 import { useState, useEffect } from "react";
+import { useDialog } from "@/context/dialogContext";
 
 export default function Column({ column }: { column: Column }) {
+  const { state, setIsLoading } = useDialog();
   const [isClient, setIsClient] = useState(false);
+  const [isDraggingOver, setIsDraggingOver] = useState(false);
+
+  const handleDrop = async () => {
+    setIsDraggingOver(false);
+    console.log("Handle drop function", state.dragData);
+    if (state.dragData && column._id !== state.dragData?.column_id) {
+      setIsLoading(true);
+      await updateDropColumn(column._id, state.dragData);
+    }
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault(); // Prevent default to allow drop
+    setIsDraggingOver(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDraggingOver(false);
+  };
 
   useEffect(() => {
     setIsClient(true);
@@ -24,7 +46,15 @@ export default function Column({ column }: { column: Column }) {
           {column.column_name} ({column.tasks.length})
         </p>
       </div>
-      <div className="w-full h-full overflow-y-auto no-scrollbar pb-16">
+      <div
+        className={`w-full h-full overflow-y-auto no-scrollbar pb-16 ${
+          isDraggingOver &&
+          "bg-[#3e3f53] border-[#635FC7] border transition-all duration-75 ease-linear"
+        }`}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
         <ColumnTasks tasks={column.tasks} />
       </div>
     </div>
