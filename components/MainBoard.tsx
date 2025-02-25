b51833a163ebab099b87284c37c35fe239bf5368
immanuellking <emmaebubeegbosi@gmail.com>
2025-02-25 12:01:22 +0100

    removed isSigned in prop destructuring as we are not using it again

--- components/MainBoard.tsx
+++ components/MainBoard.tsx
@@ -1,17 +1,11 @@
 import BoardContainer from "./BoardContainer";
 import BoardHeader from "./BoardHeader";
 
-export default function MainBoard({
-  isSignedIn,
-  boardData,
-}: {
-  isSignedIn: boolean;
-  boardData: BoardData[];
-}) {
+export default function MainBoard({ boardData }: { boardData: BoardData[] }) {
   return (
     <main className="w-full sm:w-[75%] lg:w-[82%] flex-grow h-screen flex flex-col">
-      <BoardHeader boardData={boardData}/>
-      <BoardContainer isSignedIn={isSignedIn} boardData={boardData} />
+      <BoardHeader boardData={boardData} />
+      <BoardContainer boardData={boardData} />
     </main>
   );
 }
