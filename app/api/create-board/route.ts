447c4a17f219255ce64c7477a6a21f7b74491556
immanuellking <emmaebubeegbosi@gmail.com>
2025-02-26 23:55:04 +0100

    create board route handler working now

---
+++ app/api/create-board/route.ts
@@ -0,0 +1,88 @@
+import Board from "@/models/Board";
+import Column from "@/models/Column";
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function OPTIONS() {
+  return NextResponse.json(
+    {},
+    {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Allow all origins
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS", // Allowed methods
+        "Access-Control-Allow-Headers": "Content-Type, Authorization", // Allowed headers
+      },
+    }
+  );
+}
+
+export async function POST(request: NextRequest) {
+  const values = await request.json();
+
+  const { userId, board_name, columns } = values;
+  try {
+    const board = await Board.create({
+      board_name: board_name.trim(),
+      user_id: userId,
+    });
+
+    const columnIds = await createdColumns(columns, userId, board._id);
+
+    await Board.findByIdAndUpdate(board._id, {
+      columns: columnIds,
+    });
+
+    const updatedUser = await User.findOneAndUpdate(
+      { clerkId: userId },
+      { $push: { boards: board._id } },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      throw new Error("Failed to update the user with the new board.");
+    }
+
+    return NextResponse.json(
+      { success: true, message: "Board created successfully.", board },
+      {
+        status: 201,
+        headers: {
+          "Access-Control-Allow-Origin": "*", // Allow requests from anywhere
+        },
+      }
+    );
+  } catch (error) {
+    console.error("Error Creating/Updating board:", error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        error:
+          error instanceof Error ? error.message : "An unknown error occurred.",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+async function createdColumns(
+  columns: { column_name: string }[],
+  user_id: string,
+  board_id: string
+) {
+  const createdColumns = await Promise.all(
+    columns.map((column) =>
+      Column.create({
+        column_name: column.column_name.trim(),
+        user_id: user_id,
+        board_id: board_id,
+        tasks: [],
+      })
+    )
+  );
+
+  const columnIds = createdColumns.map((column) => column._id);
+
+  return columnIds;
+}
